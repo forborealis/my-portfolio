800fd79d3a5474b2f772105cfde1849dddbf197c
forborealis <hannahaurora.busto@tup.edu.ph>
2026-09-16 16:05:27 +0800

    minor change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import profileImage from '../assets/images/corporateImage.jpg';
+import profileImage from '../assets/images/corporateImage.JPG';
 
 const Hero = () => {
   const [isTextVisible, setIsTextVisible] = useState(false);
